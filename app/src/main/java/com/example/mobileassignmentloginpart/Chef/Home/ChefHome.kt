85f6d304a694f8077ec47b4757ab4cf80679950a
Ivan <ivan1128lazy@gmail.com>
2026-08-02 01:04:48 +0800

    Add Chef Home page Enhance Login fun Add cloudinary .....

--- app/src/main/java/com/example/mobileassignmentloginpart/Chef/Home/ChefHome.kt
+++ app/src/main/java/com/example/mobileassignmentloginpart/Chef/Home/ChefHome.kt
@@ -167,7 +167,7 @@ fun ChefHomeScreenContent(
         // Search Bar
         item {
             OutlinedTextField(
-                value = "H",
+                value = "",
                 onValueChange = {},
                 placeholder = { Text("Search dishes, events, clients...") },
                 leadingIcon = { Icon(painter = painterResource(R.drawable.ic_vertical_more), contentDescription = "Search") },
