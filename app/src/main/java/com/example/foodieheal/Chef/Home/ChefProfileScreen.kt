1d6e89638c61a27d007ad2df2bf270b7951cc434
Ivan <ivan1128lazy@gmail.com>
2026-08-02 02:01:39 +0800

    Complete Merge with master Branch add small potato feature

--- app/src/main/java/com/example/foodieheal/Chef/Home/ChefProfileScreen.kt
+++ app/src/main/java/com/example/foodieheal/Chef/Home/ChefProfileScreen.kt
@@ -22,15 +22,9 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
