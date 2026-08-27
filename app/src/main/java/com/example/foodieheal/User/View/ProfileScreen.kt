3d99dc251ffbbb13f5a02bfebd4944340cb598c6
Ivan <ivan1128lazy@gmail.com>
2026-08-27 22:18:48 +0800

    Enhance AppointmentScreen.kt

--- app/src/main/java/com/example/foodieheal/User/View/ProfileScreen.kt
+++ app/src/main/java/com/example/foodieheal/User/View/ProfileScreen.kt
@@ -50,6 +50,7 @@ import com.example.foodieheal.Recipe.View.RecipeCardItem
 import com.example.foodieheal.Recipe.View.FilterSectionHeader
 import com.example.foodieheal.Chef.model.Chef
 import com.example.foodieheal.Chef.ViewModel.Register.ChefRegisterViewModel
+import com.example.foodieheal.User.Model.User
 import com.example.foodieheal.hiring.components.ActiveFiltersRow
 import com.example.foodieheal.hiring.components.ChefFilterBottomSheet
 import com.example.foodieheal.hiring.components.ChefFilterState
